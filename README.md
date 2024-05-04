6dc5dbd7a52838cd8cfa53e1fa20d4b59c0117f1
Poletansky Viktor <poletansky@gmail.com>
2024-05-04 20:08:35 +0300

    [master] implement server

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # imhttp
 
-Immediate Mode HTTP Client Library.
+Immediate Mode HTTP Server Library.
+
+> Actually now it just sends all requests to single channel still spawning goroutine for each incoming request. Fair implementation would require to parse http from each incoming connection while handling chunking and exceptional cases which is quite hard for me to implement without premade libraries.
+
+See [main.go](./cmd/main.go) for example usage.
--- cmd/main.go
+++ cmd/main.go
@@ -2,67 +2,39 @@ package main
 
 import (
 	"fmt"
-	"net"
+	"log"
+	"net/http"
+	"net/http/httputil"
 	"os"
 	"strconv"
 
-	. "github.com/rprtr258/imhttp"
+	"github.com/rprtr258/imhttp"
 )
 
-func run(host string, port int, path string) error {
-	addrs, err := net.ResolveTCPAddr("tcp", host+":"+strconv.Itoa(port))
-	if err != nil {
-		return fmt.Errorf("Could not get address of %q: %w", host, err)
-	}
-
-	conn, err := net.DialTCP("tcp", nil, addrs)
-	if err != nil {
-		return fmt.Errorf("Could not connect to %s:%d: %w", host, port, err)
-	}
-	defer conn.Close()
-
-	imhttp := New(conn)
-
-	imhttp.Req(IMHTTP_GET, path).
-		ReqHeaders(map[string]string{
-			"Host":  host,
-			"Foo":   "Bar",
-			"Hello": "World",
-		}).
-		ReqBodyChunk("Hello, World\n").
-		ReqBodyChunk("Test, test, test\n")
-
-	imhttp.ResBegin()
-	{
-		fmt.Printf("Status Code: %d\n", imhttp.ResStatusCode())
-
-		// Headers
-		var name, value []byte
-		for imhttp.ResNextHeader(&name, &value) {
-			fmt.Printf("------------------------------\n")
-			fmt.Printf("Header Name: %s\n", name)
-			fmt.Printf("Header Value: %s\n", value)
-		}
-		fmt.Printf("------------------------------\n")
-
-		// Body
-		var chunk []byte
-		for imhttp.ResNextBodyChunk(&chunk) {
-			fmt.Printf("%s", chunk)
-		}
-	}
-	imhttp.ResEnd()
-
-	return nil
-}
-
 func main() {
-	if err := run(
-		// TODO: Sometimes http://anglesharp.azurewebsites.net/Chunked fires the asserts
-		// "anglesharp.azurewebsites.net", 80, "/Chunked",
-		"google.com", 80, "/",
-	); err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		os.Exit(1)
+	// no synchronization at all!
+	counters := map[string]int{}
+	addr := ":8080"
+	fmt.Fprintln(os.Stderr, "listening on", addr)
+	for req := range imhttp.Run(addr) {
+		switch req.URL.Path {
+		case "/get":
+			name := req.URL.Query().Get("name")
+			fmt.Fprintln(req.Response, counters[name])
+		case "/set":
+			name := req.URL.Query().Get("name")
+			value, _ := strconv.Atoi(req.URL.Query().Get("value"))
+			counters[name] = value
+		case "/inc":
+			name := req.URL.Query().Get("name")
+			counters[name]++
+		default:
+			b, _ := httputil.DumpRequest(req.Request, true)
+			log.Println("REQUEST:")
+			log.Println(string(b))
+			http.NotFound(req.Response, req.Request)
+		}
+		// don't forget to call Done or connection will leak
+		req.Done()
 	}
 }
