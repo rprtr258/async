ec278d64598f6b4f29d4518fb2c38057d74c14ef
Poletansky Viktor <poletansky@gmail.com>
2024-07-20 11:51:26 +0300

    [dev] add simplest immediate json parser

--- json/json.go
+++ json/json.go
@@ -1,11 +1,130 @@
 package json
 
-import (
-	"encoding/json"
+import "fmt"
+
+type Kind int8
+
+const (
+	KindError Kind = iota
+	KindObjectBegin
+	KindObjectKey
+	KindObjectEnd
+	KindArrayBegin
+	KindArrayEnd
+	KindNull
+	KindTrue
+	KindFalse
+	KindNumber
+	KindString
 )
 
-func parseJSON(data []byte) (map[string]any, error) {
-	obj := map[string]any{}
-	err := json.Unmarshal(data, &obj)
-	return obj, err
+type parser struct {
+	pos   int
+	b     []byte
+	yield func(b []byte, kind Kind)
+}
+
+func (p *parser) skip(s string) {
+	for i := 0; i < len(s); i++ {
+		if p.b[p.pos+i] != s[i] {
+			panic(fmt.Sprintf("%s %q", string(p.b[p.pos:]), s))
+		}
+	}
+	p.pos += len(s)
+}
+
+func (p *parser) exact(s string, kind Kind) {
+	res := p.b[p.pos : p.pos+len(s)]
+	p.yield(res, kind)
+	p.skip(s)
+}
+
+func (p *parser) number() {
+	for i := 0; ; i++ {
+		switch p.b[p.pos+i] {
+		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+		default:
+			res := p.b[p.pos:][:i]
+			p.yield(res, KindNumber)
+			p.pos += i
+			return
+		}
+	}
+}
+
+func (p *parser) string(kind Kind) {
+	i := 1 // skip "
+	for {
+		if p.b[p.pos+i] == '\\' {
+			i++
+		} else if p.b[p.pos+i] == '"' {
+			break
+		}
+		i++
+	}
+	p.yield(p.b[p.pos:][:i+1], kind)
+	p.pos += i + 1
+}
+
+func (p *parser) array() {
+	p.exact("[", KindArrayBegin)
+	for {
+		p.json()
+		if p.b[0] == ']' {
+			p.exact("]", KindArrayEnd)
+			break
+		} else {
+			p.skip(",")
+		}
+	}
+}
+
+func (p *parser) object() {
+	p.exact("{", KindObjectBegin)
+	for {
+		p.string(KindObjectKey)
+		p.skip(":")
+		p.json()
+		if p.b[p.pos] == '}' {
+			p.exact("}", KindObjectEnd)
+			break
+		} else {
+			p.skip(",")
+		}
+	}
+}
+
+func (p *parser) json() {
+	switch p.b[p.pos] {
+	case 'n':
+		p.exact("null", KindNull)
+	case 'f':
+		p.exact("false", KindFalse)
+	case 't':
+		p.exact("true", KindTrue)
+	case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-':
+		p.number()
+	case '{':
+		p.object()
+	case '[':
+		p.array()
+	case '"':
+		p.string(KindString)
+	default:
+		p.yield(p.b, KindError)
+		panic(fmt.Errorf("unexpected character %c", p.b[p.pos]))
+	}
+}
+
+func Parse(b []byte) func(func(pos int, b []byte, kind Kind)) {
+	return func(yield func(pos int, b []byte, kind Kind)) {
+		p := &parser{
+			pos: 0,
+			b:   b,
+		}
+		p.yield = func(b []byte, kind Kind) {
+			yield(p.pos, b, kind)
+		}
+		p.json()
+	}
 }
--- json/json_test.go
+++ json/json_test.go
@@ -6,40 +6,69 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type token struct {
+	pos  int
+	b    string
+	kind Kind
+}
+
+func parseJSON(s string) []token {
+	res := []token{}
+	Parse([]byte(s))(func(pos int, b []byte, kind Kind) {
+		res = append(res, token{pos, string(b), kind})
+	})
+	return res
+}
+
 func TestParseJSON(t *testing.T) {
-	tests := map[string]struct {
-		input []byte
-		want  map[string]any
+	for name, tt := range map[string]struct {
+		input string
+		want  []token
 	}{
 		"valid JSON object with string values": {
-			input: []byte(`{"name":"John","city":"New York"}`),
-			want: map[string]any{
-				"name": "John",
-				"city": "New York",
+			//                11111111112222222222333
+			//      012345678901234567890123456789012
+			input: `{"name":"John","city":"New York"}`,
+			want: []token{
+				{0, `{`, KindObjectBegin},
+				{1, `"name"`, KindObjectKey},
+				{8, `"John"`, KindString},
+				{15, `"city"`, KindObjectKey},
+				{22, `"New York"`, KindString},
+				{32, `}`, KindObjectEnd},
 			},
 		},
 		"valid JSON object with integer values": {
-			input: []byte(`{"age":30,"year":2022}`),
-			want: map[string]any{
-				"age":  30.0,
-				"year": 2022.0,
+			//                111111111122
+			//      0123456789012345678901
+			input: `{"age":30,"year":2022}`,
+			want: []token{
+				{0, `{`, KindObjectBegin},
+				{1, `"age"`, KindObjectKey},
+				{7, `30`, KindNumber},
+				{10, `"year"`, KindObjectKey},
+				{17, `2022`, KindNumber},
+				{21, `}`, KindObjectEnd},
 			},
 		},
 		"valid JSON object with mixed values": {
-			input: []byte(`{"name":"John","age":30,"city":"New York"}`),
-			want: map[string]any{
-				"name": "John",
-				"age":  30.0,
-				"city": "New York",
+			//                11111111112222222222333333333344
+			//      012345678901234567890123456789012345678901
+			input: `{"name":"John","age":30,"city":"New York"}`,
+			want: []token{
+				{0, `{`, KindObjectBegin},
+				{1, `"name"`, KindObjectKey},
+				{8, `"John"`, KindString},
+				{15, `"age"`, KindObjectKey},
+				{21, `30`, KindNumber},
+				{24, `"city"`, KindObjectKey},
+				{31, `"New York"`, KindString},
+				{41, `}`, KindObjectEnd},
 			},
 		},
-	}
-
-	for name, tt := range tests {
+	} {
 		t.Run(name, func(t *testing.T) {
-			got, err := parseJSON(tt.input)
-			require.NoError(t, err)
-			require.Equal(t, tt.want, got)
+			require.Equal(t, tt.want, parseJSON(tt.input))
 		})
 	}
 }
