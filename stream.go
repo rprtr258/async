8fb8ee51d777b710159dc4a8eddcc9dcce282a69
Poletansky Viktor <poletansky@gmail.com>
2024-07-22 12:52:17 +0300

    [dev] add NewSelectAll

--- stream.go
+++ stream.go
@@ -1,5 +1,7 @@
 package imhttp
 
+import "slices"
+
 // actually is just a channel
 type Stream[T any] struct {
 	ch <-chan T
@@ -32,6 +34,37 @@ func NewInfinite[T any](next func() T) Stream[T] {
 		}
 	}()
 	return Stream[T]{ch}
+}
+
+func NewSelectAll[T any](ss ...Stream[T]) Stream[T] {
+	ch := make(chan T)
+	go func() {
+		futures := make([]Future[Option[T]], len(ss))
+		for i, s := range ss {
+			futures[i] = s.Next()
+		}
+
+		for { // loop, yielding values
+			if len(ss) == 0 {
+				break
+			}
+
+		RETRY: // loop yielding single value among closed streams
+			i, f := Select(futures...)
+			if !f.Valid {
+				// stream closed, remove it and retry
+				ss = slices.Delete(ss, i, i+1)[:len(ss)-1]
+				futures = slices.Delete(futures, i, i+1)[:len(futures)-1]
+				goto RETRY
+			}
+
+			// got value, produce it and get new one in place of old one
+			ch <- f.Value
+			futures[i] = ss[i].Next()
+		}
+		close(ch)
+	}()
+	return NewStream(ch)
 }
 
 type Option[T any] struct {
